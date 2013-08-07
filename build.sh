c051244ca6a4e264d4f2313bf550fd87518801c3
Perka <mrperkarlsson@gmail.com>
2013-08-07 03:23:24 +0200

    config

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export export CROSS_COMPILE=~/kernel/toolchains/arm-unknown-linux-gnueabi-4.7.4/bin/arm-gnueabi-
+export CROSS_COMPILE=/home/perka/kernel/toolchains/arm-unknown-linux-gnueabi-4.7.4/bin/arm-gnueabi-
 make mrproper
 make zram_shark_defconfig
 time make -j3
